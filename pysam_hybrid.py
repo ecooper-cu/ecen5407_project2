46e8cf08cc41b8cc9c65c7306783b9499ce3cf36
Anna Van Boven <anna.van.boven@gmail.com>
2024-11-04 10:27:41 -0700

    combine leap and no leap

--- pysam_hybrid.py
+++ pysam_hybrid.py
@@ -2,7 +2,8 @@
 import PySAM.Pvwattsv8 as pv_model
 import PySAM.Windpower as wind_model
 import PySAM.Battery as battery_model
-
+import pandas as pd
+import os
 from PySAM.Hybrids.HybridSystem import HybridSystem
 
 import json # To load inputs from SAM
@@ -38,6 +39,11 @@ windannualenergy = m.wind.Outputs.annual_energy
 battrountripefficiency = m.battery.Outputs.average_battery_roundtrip_efficiency
 gridannualenergy = m._grid.SystemOutput.annual_energy
 npv = m.singleowner.Outputs.project_return_aftertax_npv
+windenergytime = m.wind.Outputs.annual_energy_distribution_time # time series of wind production
+columns = windenergytime[0][1:] 
+row_indices = [row[0] for row in windenergytime[1:]]  
+values = [row[1:] for row in windenergytime[1:]]      
+windseries_df = pd.DataFrame(values, columns=columns, index=row_indices) # df of windseries
 
 # print outputs
 print(pvannualenergy)
@@ -45,4 +51,22 @@ print(windannualenergy)
 print(battrountripefficiency)
 print(gridannualenergy)
 print(npv)
+# %% create same model with leap year data
+m = HybridSystem([pv_model, wind_model, battery_model], 'singleowner')
+m.new()
+inputs['windpower']['wind_resource_filename'] = os.path.join('data', 'wind_speeds', 'sd_2012_5m_leap.csv')
+unassigned = m.assign(inputs) # returns a list of unassigned variables if any
+print(unassigned)
+m.execute()
+# %% pull leap year data & combine into single dataframe
+windenergytime_1 = m.wind.Outputs.annual_energy_distribution_time # time series of wind production
+columns = windenergytime[0][1:] 
+row_indices = [row[0] for row in windenergytime[1:]]  
+values = [row[1:] for row in windenergytime[1:]]      
+windseries_df_1 = pd.DataFrame(values, columns=columns, index=row_indices) # df of windseries
+cols = range(1, 367)
+
 # %%
+windseries_df.insert(31+28, 368, windseries_df_1[31+28].values)
+windseries_df.columns = cols
+#%%
