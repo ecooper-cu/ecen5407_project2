1fe4c72ee1d9730e26d8c24deb4c9de13eca3e69
Rob Stallman <robstallman3@gmail.com>
2024-11-13 12:36:56 -0700

    Move leap day analysis to a separate script so that pysam_hybrid is just for generating hybrid system outputs

--- archived/leap_analysis.py
+++ archived/leap_analysis.py
@@ -72,15 +72,44 @@ pysam_helpers.plot_values_by_time_range(df=wind_model_outputs['5 Minute Data'], 
 pysam_helpers.plot_values_by_time_range(df=battery_model_outputs['Lifetime 5 Minute Data'], start_time=date_start, end_time=date_end, y_columns=['batt_SOC'])
 pysam_helpers.plot_values_by_time_range(df=battery_model_outputs['Lifetime 5 Minute Data'], start_time=date_start, end_time=date_end, y_columns=['batt_to_grid', 'system_to_batt', 'system_to_grid'])
 
-# %% Inspect model outputs
+# %% Re-run the model using data with the Feb 29 values subbed in for Feb 28 values.
+# We are using input files which represent the resource availability from the year 2012. However, 
+# since 2012 is a leap year, SAM won't manage the leap day entries well. As such, the default 
+# data (used above) has been modified to eliminate all entries from Feb 29, 2012. 
+# Using that data provided us with a baseline for the expected plant output over the course of a 
+# non-leap year. We now wish to use data where Feb 28 entries are replaced by values from Feb 29.
+# The model outputs using that data can provide information on the expected output during a leap 
+# day.
+
+m_leap = HybridSystem([pv_model, wind_model, battery_model], 'singleowner')
+m_leap.new()
+
+unassigned = m_leap.assign(inputs)
+print(unassigned)
+
+m_leap.pv.SolarResource.solar_resource_file = 'data/222628_32.73_-117.18_2022_interpolated_LEAP.csv'
+m_leap.wind.Resource.wind_resource_filename = 'data/wind_speeds/sd_2012_5m_leap.csv'
+
+# Change the minimum battery SoC to 10%
+m_leap.battery.value("batt_minimum_SOC", 10)
+
+m_leap.execute()
+
+pv_model_leap_outputs = pysam_helpers.parse_model_outputs_into_dataframes(m_leap.pv)
+wind_model_leap_outputs = pysam_helpers.parse_model_outputs_into_dataframes(m_leap.wind)
+battery_model_leap_outputs = pysam_helpers.parse_model_outputs_into_dataframes(m_leap.battery)
+grid_model_leap_outputs = pysam_helpers.parse_model_outputs_into_dataframes(m_leap._grid)
+single_owner_leap_outputs = pysam_helpers.parse_model_outputs_into_dataframes(m_leap.singleowner)
+
+# %% Find the Feb 29 energy production as a share of annual energy production
 # First, pull the relevant columns from each module output DataFrame into one DataFrame for the 
 # system output
-pv_df = pv_model_outputs['Lifetime 5 Minute Data'].reset_index()
+pv_df = pv_model_leap_outputs['Lifetime 5 Minute Data'].reset_index()
 pv_df = pv_df.loc[:,['Datetime', 'subarray1_dc_gross', 'ac_gross']]
 pv_df.rename(columns={'subarray1_dc_gross':'PV DC Gross (kW)', 'ac_gross': 'PV AC Gross (kW)'}, inplace=True)
-batt_df = battery_model_outputs['Lifetime 5 Minute Data'].reset_index()
+batt_df = battery_model_leap_outputs['Lifetime 5 Minute Data'].reset_index()
 batt_df = batt_df.loc[:,['Datetime', 'batt_to_grid', 'system_to_batt', 'system_to_grid']]
-wind_df = wind_model_outputs['5 Minute Data'].reset_index()
+wind_df = wind_model_leap_outputs['5 Minute Data'].reset_index()
 wind_df = wind_df.loc[:,['Datetime', 'gen']]
 wind_df.rename(columns={'gen':'Wind AC Gross (kW)'}, inplace=True)
 system_df = pd.merge(pv_df, batt_df, on='Datetime', how='inner')
@@ -103,14 +132,31 @@ date_end = '2012-07-28 00:00:00'
 system_df.set_index('Datetime', inplace=True)
 pysam_helpers.plot_values_by_time_range(df=system_df, start_time=date_start, end_time=date_end, y_columns=['PV AC Gross (kW)', 'Generation to Grid (kW)', 'Total Generation (kW)'])
 
+# Find the leap day generation
+system_df['Power to Grid (kW)'] = system_df['batt_to_grid'] + system_df['system_to_grid'] + system_df['Wind AC Gross (kW)']
+system_df['Energy to Grid (kWh)'] = system_df['Power to Grid (kW)'] * (5/60)
+# Note that the indexing is a little wonky because we're spoofing Feb 29 data as Feb 28 when 
+# running the SAM simulation
+feb_29_start = pd.Timestamp(f'2012-02-28 00:00:00')
+feb_29_end = pd.Timestamp(f'2012-03-01 00:00:00')
+system_df.reset_index(inplace=True)
+system_df['Datetime'] = pd.to_datetime(system_df['Datetime'])
+year_1_mask = system_df['Datetime'] < pd.Timestamp(f'2013-01-01 00:00:00')
+feb_29_mask = (system_df['Datetime'] >= feb_29_start) & (system_df['Datetime'] < feb_29_end)
+leap_day_generation_kwh = system_df.loc[feb_29_mask, 'Energy to Grid (kWh)'].sum()
+year_1_generation_kwh = system_df.loc[year_1_mask, 'Energy to Grid (kWh)'].sum()
+leap_day_generation_percent = (leap_day_generation_kwh / year_1_generation_kwh) * 100
+
+# Print the results
+print(f"The generation on leap day is: {leap_day_generation_kwh:.2f} kWh.")
+print(f"As a fraction of annual generation for year 1, the generation on leap day is: {leap_day_generation_percent:.2f}%")
+
 # %% Compare load to generation
 # Build a dataframe of load data
 load = load_inspection_helpers.format_load_data(load_filepath='data/Project 2 - Load Profile.xlsx')
 load['Load (kW)'] = load['Load (MW)'] * 1000
 
 # Merge the two dataframes
-system_df.reset_index(inplace=True)
-system_df['Datetime'] = pd.to_datetime(system_df['Datetime'])
 merged = pd.merge(system_df, load, on='Datetime', how='inner')
 
 merged['Excess Generation (kW)'] = merged['Generation to Grid (kW)'] - merged['Load (kW)']
@@ -129,12 +175,6 @@ merged[unmet_load_mask].plot(x='Datetime', y='Unmet Load (kW)')
 lcoe_nom = m.singleowner.Outputs.lcoe_nom
 net_cost = m.singleowner.Outputs.cost_installed
 
-# Find the year 1 generation
-system_df['Power to Grid (kW)'] = system_df['batt_to_grid'] + system_df['system_to_grid'] + system_df['Wind AC Gross (kW)']
-system_df['Energy to Grid (kWh)'] = system_df['Power to Grid (kW)'] * (5/60)
-year_1_mask = system_df['Datetime'] < pd.Timestamp(f'2013-01-01 00:00:00')
-year_1_generation_kwh = system_df.loc[year_1_mask, 'Energy to Grid (kWh)'].sum()
-
 # Calculate some metrics on the excess generation
 curtailment_mask = merged['Excess Generation (kW)'] > unmet_threshold
 curtailment_instances = curtailment_mask.sum()
@@ -150,13 +190,10 @@ print(f"The installed cost of the system is: ${net_cost:.2e}")
 print(f"The calculated nominal LCOE is: {(lcoe_nom/100):.2f} $/kWh")
 
 # Plot some results
-# This plot demonstrates the dispatch stack during the highest load day (June 27th)
 date_start = '2012-07-27 00:00:00'
 date_end = '2012-07-28 00:00:00'
 merged.set_index('Datetime', inplace=True)
 pysam_helpers.plot_values_by_time_range(df=merged, start_time=date_start, end_time=date_end, y_columns=['Load (kW)', 'Generation to Grid (kW)', 'Total Generation (kW)'])
-
-# These plots demonstrates the dispatch stack during a day where we fail to meet the load
 date_start = '2012-12-19 00:00:00'
 date_end = '2012-12-20 00:00:00'
 pysam_helpers.plot_values_by_time_range(df=merged, start_time=date_start, end_time=date_end, y_columns=['Load (kW)', 'Generation to Grid (kW)', 'Total Generation (kW)'])
